79151ccbfa5b5ffe784f7c1a44fd7067c89542bb
Austin Hartzheim <austin@austinhartzheim.me>
2015-12-01 05:12:37 +0000

    Removed debug messages.

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ exports.containsSuspiciousCharacter = containsSuspiciousCharacter;
 
 // Check a tab object for a suspicious URL and display warnings
 function checkTab(tab) {
-    console.log(tab.url);
     if (containsSuspiciousCharacter(tab.url)) {
-        console.log("WARNING: Suspicious URL required.");
         var warningPanel = panel.Panel({
             width: 533,
             height: 200,
@@ -21,11 +19,9 @@ function checkTab(tab) {
             contentScriptFile: self.data.url('warning.js')
         });
         warningPanel.port.on('close', function(data) {
-            console.log('Attempting to destroy port.');
             warningPanel.destroy();
         });
         warningPanel.port.on('leave', function(data) {
-            console.log(tab);
             tab.url = self.data.url('explain.html');
             warningPanel.destroy();
         });
